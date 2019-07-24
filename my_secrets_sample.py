6a422e654f1e10ee32405802fd38665bb60a2614
rocketmovingapp <rocketmovinginc@gmail.com>
2019-07-24 02:11:22 -0400

    Sample My Secrets file

---
+++ my_secrets_sample.py
@@ -0,0 +1,7 @@
+"""Module containing credentials."""
+
+# Google Sheets
+gspread_id = "" # TODO Enter the Google Sheets Spreadsheet ID
+
+# Google Maps
+ggeocode = "" # TODO Enter the Google Maps API Key
