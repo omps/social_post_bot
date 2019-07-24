38fde5f0328530fafd396e70ed39946b99fee9fb
rocketmovingapp <rocketmovinginc@gmail.com>
2019-07-24 02:42:11 -0400

    Added post_size_guide

--- my_secrets_sample.py
+++ my_secrets_sample.py
@@ -5,3 +5,11 @@ gspread_id = "" # TODO Enter the Google Sheets Spreadsheet ID
 
 # Google Maps
 ggeocode = "" # TODO Enter the Google Maps API Key
+
+# Last updated: May 20, 2019 - From istockphoto.com
+post_size_guide = {
+   'facebook_post': (940, 788),
+   'instagram_post': (1080, 1080),
+   'twitter_post': (1024, 512),
+   'pinterest_post': (735, 1102),
+}   
