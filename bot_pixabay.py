caceec8f0ccd10312e19d12e21338590d792f504
rocketmovingapp <rocketmovinginc@gmail.com>
2019-07-25 01:48:37 -0400

    Updates pixabay

--- bot_pixabay.py
+++ bot_pixabay.py
@@ -25,20 +25,33 @@ def get_image_from_pixabay(image_id, sm_account):
 
     def get_pixabay_large_image(js, image_id):
         """Function to get the image from the url."""
+        # Get the hit
+        hit = js.get("hits")[0]
+        hit_id = hit["id"]
+        # Make sure you have the correct image
+        if str(hit_id) == (image_id):
+            hit_link = hit["largeImageURL"]
+            print(f"Found image {hit_id} with URL {hit_link}")
+
+        return hit_id, hit_link
+
+    def save_image(hit_id, hit_link):
+        """Get the image from the url."""
         image = requests.get(hit_link)
         #  Print the status code
         if image.status_code == 200:
             print(f"Status code {str(image.status_code)}")
             # Get the content
-            with open(f"{sm_account}_img_original_{str(hit_id)}.jpg", "wb") as f:
+            with open(f"{sm_account}-img-original-{str(hit_id)}.jpg", "wb") as f:
                 f.write(requests.get(hit_link).content)
                 f.close()
             os.chdir("..")
             print("Image saved.")
+            
 
-        # Get the json from the API
-        js = get_pixabay_url(url=URL, image_id=image_id)
-        # Get the image details
-        hit_id, hit_link = get_pixabay_large_image(js=js, image_id=image_id)
-        # Get the image
-        image_content = save_image(hit_id=hit_id, hit_link=hit_link)
+    # Get the json from the API
+    js = get_pixabay_url(url=URL, image_id=image_id)
+    # Get the image details
+    hit_id, hit_link = get_pixabay_large_image(js=js, image_id=image_id)
+    # Get the image
+    image_content = save_image(hit_id=hit_id, hit_link=hit_link)
